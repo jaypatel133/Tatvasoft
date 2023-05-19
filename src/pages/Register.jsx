e200812e8dc23e89b2ec16dd11d448d2d8493215
jaypatel133 <67822461+jaypatel133@users.noreply.github.com>
2023-05-19 16:58:14 +0530

    register added

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,61 @@
 import React from 'react';
+import './Register.css';
+import {Button} from '@mui/material';
 
-function Login(props) {
+const styles = {
+    can_b:{
+        "&.MuiButton-root": {
+            border: "1px solid #f14d54",
+            bgcolor: "#f14d54",
+          },
+          "&.MuiButton-contained": {
+            color: "white",
+          }
+    }
+        
+  };
+
+
+function Register(props) {
     return (
         <div>
-            Register Page
+            <div className='redir'>
+                Home /<span> Create an Account</span>
+            </div>
+
+            <div className='loginTitle'>
+                Login or Create an Account
+            </div>
+
+            <div style={{display:"block",marginTop:"50px",marginLeft:"6rem",marginRight:"6rem"}}>
+                <h2 style={{fontSize:"20px",fontWeight:"500",marginBottom:"20px"}}>Personal Information</h2>
+                <h4 style={{fontSize:"15px",fontWeight:"300",marginTop:"20px",marginBottom:"20px"}}>Please enter the following Information to create your Account</h4>
+                <div style={{width:"49%",height:"80px",display:"inline-block"}}>
+                    <samp style={{display:"block"}}>Email Address:</samp>
+                    <input style={{width:"100%",height:"40px"}} placeholder='email'/>
+                </div>
+                <div style={{width:"49%",height:"80px",display:"inline-block",marginLeft:"2%"}}>
+                    <samp style={{display:"block"}}>Email Address:</samp>
+                    <input style={{width:"100%",height:"40px"}} placeholder='email'/>
+                </div>
+                <samp style={{display:"block"}}>Email Address:</samp>
+                <input style={{width:"100%",height:"40px"}} placeholder='email'/>
+                
+                <h2 style={{fontSize:"20px",fontWeight:"500",marginTop:"70px",marginBottom:"20px"}}>Login Information</h2>
+                <div style={{width:"49%",height:"80px",display:"inline-block"}}>
+                    <samp style={{display:"block"}}>Email Address:</samp>
+                    <input style={{width:"100%",height:"40px"}} placeholder='email'/>
+                </div>
+                <div style={{width:"49%",height:"80px",display:"inline-block",marginLeft:"2%"}}>
+                    <samp style={{display:"block"}}>Email Address:</samp>
+                    <input style={{width:"100%",height:"40px"}} placeholder='email'/>
+                </div>
+
+                <Button sx={styles.can_b} variant="contained" className='nav_b' >Register </Button>
+            
+            </div>
         </div>
     );
 }
 
-export default Login;
+export default Register;
