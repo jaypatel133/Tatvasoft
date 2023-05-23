d29eb428ff2d3c824993de37198f3e56642efca3
jaypatel133 <67822461+jaypatel133@users.noreply.github.com>
2023-05-23 12:05:46 +0530

    Added Autocomplete in search bar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
-import { TextField,Button} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button} from '@mui/material';
 import {Search} from '@mui/icons-material';
 import './SearchBar.css'
-import { bgcolor } from '@mui/system';
+import useAutocomplete from '@mui/base/useAutocomplete';
+import { styled } from '@mui/system';
+
+const Listbox = styled('ul')(({ theme }) => ({
+  width: "422px",
+  margin: 0,
+  padding: "5px",
+  zIndex: 1,
+  position: 'absolute',
+  listStyle: 'none',
+  backgroundColor: theme.palette.mode === 'light' ? '#fff' : '#000',
+  overflow: 'auto',
+  maxHeight: 200,
+  border: '1px solid rgba(0,0,0,.25)',
+}));
 
 const styles = {
     sea_b:{
@@ -30,13 +44,71 @@ const styles = {
 
 
 function SearchBar(props) {
+
+  const {
+    getRootProps,
+    getInputProps,
+    getListboxProps,
+    getOptionProps,
+    groupedOptions,
+  } = useAutocomplete({
+    id: 'use-autocomplete-demo',
+    options: top10Films,
+    getOptionLabel: (option) => option.title,
+    onOpen: () => SetFocus(true),
+    onClose:()=>SetFocus(false)
+  });
+
+  const[focus,SetFocus] = new useState(false);
+
+useEffect(()=>{
+  console.log(focus)
+},[focus])
     return (
-        <div className='searchBar'>
-            <input className='se' placeholder='search'/>
-            <Button sx={styles.sea_b} variant="contained" className='nav_b' startIcon={<Search/>}>Search</Button>
-            <Button sx={styles.can_b} variant="contained" className='nav_b' >Cancel </Button>
+        <div className='searchBar' >
+          <div  {...getRootProps()}>
+          <input {...getInputProps()} className='se' placeholder='search'/>
+              {groupedOptions.length > 0 ? (
+                <Listbox {...getListboxProps()}>
+                  {groupedOptions.map((option, index) => (
+                    <div {...getOptionProps({ option, index })}>
+                    <div style={{display:"block",width:"100%"}}>
+                      <div style={{display:"inline-block",width:"75%"}}>{option.title}</div>
+                      <div style={{display:"inline-block",textAlign:"right",width:"25%"}}>{option.year}</div>
+                    </div>
+                    <div style={{display:"block",width:"100%"}}>
+                      <div style={{display:"inline-block",width:"75%"}}>category</div>
+                      <div style={{display:"inline-block",textAlign:"right",width:"25%"}}>Add to cart</div>
+                    </div>
+                    <li  style={{display:"block",width:"300px"}}>Description</li>
+                    <hr/>
+                    </div>
+                  ))}
+                </Listbox>
+              ) : focus? (<Listbox><li  style={{display:"block",width:"300px"}}>No Product Found</li></Listbox>) : null}
+              
+          </div>
+           
+          <Button sx={styles.sea_b} variant="contained" className='nav_b' startIcon={<Search/>}>Search</Button>
+          <Button sx={styles.can_b} variant="contained" className='nav_b' >Cancel </Button>
         </div>
     );
 }
+
+const top10Films = [
+  { title: 'The Shawshank Redemption', year: 1994 },
+  { title: 'The Godfather', year: 1972 },
+  { title: 'The Godfather: Part II', year: 1974 },
+  { title: 'The Dark Knight', year: 2008 },
+  { title: '12 Angry Men', year: 1957 },
+  { title: "Schindler's List", year: 1993 },
+  { title: 'Pulp Fiction', year: 1994 },
+  {
+    title: 'The Lord of the Rings: The Return of the King',
+    year: 2003,
+  },
+  { title: 'The Good, the Bad and the Ugly', year: 1966 },
+  { title: 'Fight Club', year: 1999 }
+];
 
 export default SearchBar;
